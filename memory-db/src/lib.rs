58d2050af371fbd404498641a9f1b1e1b61801a7
Michal Kucharczyk <1728078+michalkucharczyk@users.noreply.github.com>
2025-09-16 13:17:23 +0200

    MemDB: get_count added

--- memory-db/src/lib.rs
+++ memory-db/src/lib.rs
@@ -19,7 +19,7 @@
 #[cfg(not(feature = "std"))]
 extern crate alloc;
 
-use core::hash::BuildHasher;
+use core::{cell::RefCell, hash::BuildHasher};
 use hash_db::{
 	AsHashDB, AsPlainDB, HashDB, HashDBRef, Hasher as KeyHasher, MaybeDebug, PlainDB, PlainDBRef,
 	Prefix,
@@ -93,6 +93,7 @@ where
 	hashed_null_node: H::Out,
 	null_node_data: T,
 	_kf: PhantomData<KF>,
+	pub get_count: RefCell<usize>,
 }
 
 impl<H, KF, T, S> Clone for MemoryDB<H, KF, T, S>
@@ -108,6 +109,7 @@ where
 			hashed_null_node: self.hashed_null_node,
 			null_node_data: self.null_node_data.clone(),
 			_kf: Default::default(),
+			get_count: Default::default(),
 		}
 	}
 }
@@ -317,6 +319,7 @@ where
 			hashed_null_node: H::hash(null_key),
 			null_node_data,
 			_kf: Default::default(),
+			get_count: Default::default(),
 		}
 	}
 
@@ -490,6 +493,7 @@ where
 	S: BuildHasher + Default + Send + Sync,
 {
 	fn get(&self, key: &H::Out, prefix: Prefix) -> Option<T> {
+		*self.get_count.borrow_mut() += 1;
 		if key == &self.hashed_null_node {
 			return Some(self.null_node_data.clone())
 		}
